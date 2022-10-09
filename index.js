b5c1cd3e69c4d13e9c8361dd1d178e615aff4dbd
Cristianortiz19 <cristclar012@gmail.com>
2022-10-09 17:02:04 -0500

    creando puerto serial

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
-const { response } = require('express');
-const express = require('express');
+ const express = require('express');
 const { Server } = require('socket.io');
+const {SerialPort, ReadlineParser} = require('serialport');
+const cors = require('cors');
 const PORT = 5050;
-const SERVER_IP = '192.168.1.59';
 
+// ⚙️ HTTPS COMMUNICATION SETUP________________________________________________
 const app = express();
 app.use(express.json());
 app.use('/app', express.static('public-app'));
 app.use('/mupi', express.static('public-mupi'));
+//============================================ END
 
+// ⚙️ SERIAL COMMUNICATION SETUP________________________________________________
+const protocolConfiguration = {
+    path: '/COM3',
+    baudRate: 9600
+}
+const port = new SerialPort(protocolConfiguration);
+const parser = port.pipe(new ReadlineParser);
+//============================================ END
+
+// ⚙️ WEBSOCKET COMMUNICATION SETUP________________________________________________
 const httpServer = app.listen(PORT, () => {
     console.table(
         {
-            'App:' : `http://${SERVER_IP}:${PORT}/app`,
-            'Mupi:' : `http://${SERVER_IP}:${PORT}/mupi`,
+            'App:' : `http://localhost:${PORT}/app`,
+            'Mupi:' : `http://localhost:${PORT}/mupi`,
         }
     )
-});
-
+}); 
 const io = new Server(httpServer, { path: '/real-time' });
+//============================================ END
 
+// 🔄 SERIAL COMMUNICATION WORKING___________________________________________
+parser.on('data', (arduinoData) => {
+    let dataArray = arduinoData.split(' ');
+    console.log(dataArray);
+})
+
+
+// 🔄 WEBSOCKET COMMUNICATION___________________________________________
 io.on('connection', socket => {
     console.log('Conectado', socket.id);
 
